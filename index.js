d335ee0ae88636f7569499c72b44652800a529b6
Jonathan Cady <joncady@uw.edu>
2019-01-27 11:51:42 -0800

    async?

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var serviceRegion = "westus2"; // e.g., "westus"
 // now create the audio-config pointing to our stream and
 // the speech config specifying the language.
 
-app.post("/audio", upload.single('data'), (req, res) => {
+app.post("/audio", upload.single('data'), async (req, res) => {
     var pushStream = sdk.AudioInputStream.createPushStream();
     let file = req.file;
     fs.createReadStream(file.path).on('data', function (arrayBuffer) {
@@ -58,14 +58,14 @@ app.post("/audio", upload.single('data'), (req, res) => {
     var recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
     // start the recognizer and wait for a result.
-    recognizer.recognizeOnceAsync(
-        function (result) {
-            let toSend = clean.parseCommands((JSON.parse(result["privJson"])).DisplayText);
-            res.send(toSend);
-            recognizer.close();
-            recognizer = undefined;
+    let toSend = await recognizer.recognizeOnceAsync(
+        async function (result) {
+            return clean.parseCommands((JSON.parse(result["privJson"])).DisplayText);
+            // recognizer.close();
+            // recognizer = undefined;
         },
     );
+    res.send(toSend);
 });
 
 app.post('/upload', upload.single('song'), (req, res) => {
@@ -97,6 +97,3 @@ app.get('/songnames', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-
-
--- regex.js
+++ regex.js
@@ -8,11 +8,14 @@ exports.parseCommands = function(commandStr) {
     console.log(commandStr);
     let regexBarNum = /(bar|measure)\s[0-9]+/i;
     let regexStop = /stop/i;
+    let regexStart = /start/i;
     if (regexBarNum.test(commandStr)) {
         let barNum = parseInt(commandStr.match(regexBarNum)[0].match(/[0-9]+/)[0],10);
         return { type: "location", bar: barNum };
     } else if (regexStop.test(commandStr)) {
         return { type: "stop" };
+    } else if (regexStart.test(commandStr)) {
+        return { type: "start" };
     } else {
         return { type: "fail", message: "No command found." };
     }
