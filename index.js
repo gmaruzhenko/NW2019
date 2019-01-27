4aac7a4f4f289b852a124dca1d11414f0ec94e5a
Jonathan Cady <joncady@uw.edu>
2019-01-27 02:05:27 -0800

    move upload out of folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const cors = require('cors');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads')
+        cb(null, './')
     }
 });
 const fs = require('fs');
